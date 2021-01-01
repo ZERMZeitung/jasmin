8349edf3c57dd1341b868137892f44eb9c704a5d
chrissx <chrissx@chrissx.de>
2021-01-01 03:56:46 +0100

    i just noticed that the handler having a / already enforces the /

--- src/main.go
+++ src/main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"net/http"
 	"os"
+	"strings"
 
 	"github.com/gomarkdown/markdown"
 )
@@ -22,35 +23,35 @@ func main() {
 		}
 
 		path := r.RequestURI
-		if path[0] != "/"[0] {
-			path = "/" + path
-		}
-		//so far havent managed to exploit, but it should work in theory:
-		//echo 'GET /../../../etc/passwd HTTP/1.1
-		//Host: localhost:8099
-		//User-Agent: curl/7.64.1
-		//Accept: */*
-		//' |nc localhost 8099
-		//TODO: check this
-		path = fmt.Sprintf("../zerm.eu%s", path)
 
-		file, err := os.OpenFile(path, os.O_RDONLY, 0o644)
-		if err != nil {
-			internalServerError(w, err)
-			return
-		}
-		defer file.Close()
+		if strings.HasPrefix(path, "/zerm") {
+			//so far havent managed to exploit, but it should work in theory:
+			//echo 'GET /zerm/../../../../etc/passwd HTTP/1.1
+			//Host: localhost:8099
+			//User-Agent: curl/7.64.1
+			//Accept: */*
+			//' |nc localhost 8099
+			//TODO: check this
+			path = fmt.Sprintf("../zerm.eu%s", path)
 
-		stat, err := file.Stat()
-		if err != nil {
-			internalServerError(w, err)
-			return
-		}
+			file, err := os.OpenFile(path, os.O_RDONLY, 0o644)
+			if err != nil {
+				internalServerError(w, err)
+				return
+			}
+			defer file.Close()
 
-		md := make([]byte, stat.Size())
-		//TODO: err handling
-		file.Read(md)
-		w.Write(markdown.ToHTML(md, nil, nil))
+			stat, err := file.Stat()
+			if err != nil {
+				internalServerError(w, err)
+				return
+			}
+
+			md := make([]byte, stat.Size())
+			//TODO: err handling
+			file.Read(md)
+			w.Write(markdown.ToHTML(md, nil, nil))
+		}
 	})
 
 	http.ListenAndServe(":8099", nil)
