70b93d2758c80cadd214a3e65774f3ae8a44b00c
chrissx <chrissx@chrissx.de>
2021-01-01 03:34:14 +0100

    now also reading the filename out of the requested uri

--- src/main.go
+++ src/main.go
@@ -8,26 +8,45 @@ import (
 	"github.com/gomarkdown/markdown"
 )
 
+func internalServerError(w http.ResponseWriter, err error) {
+	w.WriteHeader(http.StatusInternalServerError)
+	fmt.Fprintln(w, err)
+}
+
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		if r.Method != "GET" {
-			w.WriteHeader(400)
+			w.WriteHeader(http.StatusBadRequest)
 			fmt.Fprintln(w, "WTF are you tryin to achieve?! This is GET only.")
 			return
 		}
-		file, err := os.OpenFile("README.md", os.O_RDONLY, 0o644)
+
+		path := r.RequestURI
+		if path[0] != "/"[0] {
+			path = "/" + path
+		}
+		//so far havent managed to exploit, but it should work in theory:
+		//echo 'GET /../../../etc/passwd HTTP/1.1
+		//Host: localhost:8099
+		//User-Agent: curl/7.64.1
+		//Accept: */*
+		//' |nc localhost 8099
+		//TODO: check this
+		path = fmt.Sprintf("../zerm.eu%s", path)
+
+		file, err := os.OpenFile(path, os.O_RDONLY, 0o644)
 		if err != nil {
-			w.WriteHeader(500)
-			fmt.Fprintln(w, err)
+			internalServerError(w, err)
 			return
 		}
 		defer file.Close()
+
 		stat, err := file.Stat()
 		if err != nil {
-			w.WriteHeader(500)
-			fmt.Fprintln(w, err)
+			internalServerError(w, err)
 			return
 		}
+
 		md := make([]byte, stat.Size())
 		//TODO: err handling
 		file.Read(md)
