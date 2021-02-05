07044d6fa42e2643dc0034fa1109100136e7cc92
chrissx <chrissx@chrissx.de>
2021-02-05 20:43:44 +0100

    caching for articles

--- src/main.go
+++ src/main.go
@@ -21,7 +21,9 @@ type article struct {
 	Published time.Time
 }
 
-//TODO: caching this improves performance and sounds like a good idea generally
+var allArticles []article
+var lastArticleUpdate = time.Unix(0, 0)
+
 func parseArticles() ([]article, error) {
 	file, err := os.OpenFile("../zerm.eu/articles.csv", os.O_RDONLY, 0o644)
 	if err != nil {
@@ -111,16 +113,21 @@ const logo = "<text class='logo1'>ZERM</text><text class='logo2'>ONLINE</text>"
 
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		if lastArticleUpdate.Add(1000_000000).Before(time.Now()) {
+			articles, err := parseArticles()
+			if err == nil {
+				lastArticleUpdate = time.Now()
+				allArticles = articles
+			}
+			if allArticles == nil {
+				internalServerError(w, err)
+				return
+			}
+		}
 		if r.Method != "GET" {
 			w.WriteHeader(http.StatusBadRequest)
 			fmt.Fprintln(w, "WTF are you tryin to achieve?! This is GET only.")
 		} else if r.RequestURI == "/" || strings.HasPrefix(r.RequestURI, "/index") {
-			articles, err := parseArticles()
-			if err != nil {
-				internalServerError(w, err)
-				return
-			}
-
 			fmt.Fprint(w, "<html><head>")
 			fmt.Fprint(w, "<title>ZERM Online</title>")
 			fmt.Fprint(w, "<meta charset='utf-8'/>")
@@ -135,7 +142,7 @@ func main() {
 			fmt.Fprint(w, "<a href='rss.xml'>RSS Feed</a>")
 			fmt.Fprint(w, "<ul>")
 
-			for _, article := range articles {
+			for _, article := range allArticles {
 				fmt.Fprint(w, "<li>")
 				fmt.Fprint(w, article.Published.Format("02.01.2006"))
 				fmt.Fprint(w, " &ndash; <a href='zerm/")
@@ -147,12 +154,6 @@ func main() {
 
 			fmt.Fprint(w, "</ul></body></html>")
 		} else if r.RequestURI == "/sitemap.xml" {
-			articles, err := parseArticles()
-			if err != nil {
-				internalServerError(w, err)
-				return
-			}
-
 			fmt.Fprintln(w, "<?xml version=\"1.0\" encoding=\"UTF-8\"?>")
 			fmt.Fprintln(w, "<urlset xmlns=\"http://www.sitemaps.org/schemas/sitemap/0.9\">")
 			fmt.Fprintln(w, "<url><loc>https://zerm.eu/</loc><changefreq>daily</changefreq></url>")
@@ -162,18 +163,12 @@ func main() {
 				fmt.Fprintf(w, "<url><loc>https://zerm.eu/%d.html</loc><changefreq>monthly</changefreq></url>\n", y)
 			}
 
-			for _, article := range articles {
+			for _, article := range allArticles {
 				fmt.Fprintf(w, "<url><loc>https://zerm.eu/zerm/%s.html</loc><changefreq>monthly</changefreq></url>\n", article.ID)
 			}
 
 			fmt.Fprintln(w, "</urlset>")
 		} else if r.RequestURI == "/rss.xml" {
-			articles, err := parseArticles()
-			if err != nil {
-				internalServerError(w, err)
-				return
-			}
-
 			fmt.Fprintln(w, "<?xml version=\"1.0\" encoding=\"utf-8\"?>")
 			fmt.Fprintln(w, "<?xml-stylesheet type=\"text/css\" href=\"rss.css\" ?>")
 			fmt.Fprintln(w, "<rss version=\"2.0\" xmlns:atom=\"http://www.w3.org/2005/Atom\">")
@@ -184,7 +179,7 @@ func main() {
 			fmt.Fprintln(w, "<link>https://zerm.eu/rss.xml</link>")
 			fmt.Fprintln(w, "<atom:link href=\"https://zerm.eu/rss.xml\" rel=\"self\" type=\"application/rss+xml\" />")
 
-			for _, article := range articles {
+			for _, article := range allArticles {
 				fmt.Fprintln(w, "<item>")
 				fmt.Fprintf(w, "<title>%s</title>\n", article.Title)
 				fmt.Fprintf(w, "<guid>https://zerm.eu/%d.html#%s</guid>\n", article.Published.Year(), article.ID)
@@ -202,16 +197,10 @@ func main() {
 				fmt.Fprintln(w, "]]></description>")
 			}
 		} else if strings.HasPrefix(r.RequestURI, "/zerm") {
-			articles, err := parseArticles()
-			if err != nil {
-				internalServerError(w, err)
-				return
-			}
-
 			var article article
 			found := false
 
-			for _, a := range articles {
+			for _, a := range allArticles {
 				if "/zerm/"+a.ID == r.RequestURI {
 					article = a
 					found = true
@@ -254,12 +243,6 @@ func main() {
 				return
 			}
 
-			articles, err := parseArticles()
-			if err != nil {
-				internalServerError(w, err)
-				return
-			}
-
 			fmt.Fprint(w, "<html><head><title>ZERM GA ", year)
 			fmt.Fprint(w, "</title><meta charset='utf-8'/>")
 			fmt.Fprint(w, "<link rel='stylesheet' type='text/css' href='style.css'>")
@@ -280,7 +263,7 @@ func main() {
 				fmt.Fprint(w, ".svg'>separaten Vorderseite</a>.</i></p>")
 			}
 
-			for _, article := range articles {
+			for _, article := range allArticles {
 				if article.Published.Year() != int(year) {
 					continue
 				}
