546835eee9484b983baea7292e98778706af45c7
pixel <pixel@chrissx.de>
2023-03-27 09:25:17 +0200

    simpler implementation of the logging functions

--- logging.go
+++ logging.go
@@ -1,30 +1,15 @@
 package main
 
-import (
-	"fmt"
-	"log"
-)
+import "log"
 
 func Err(v ...interface{}) {
-	msg := "[Err]"
-	for _, s := range v {
-		msg += " " + fmt.Sprint(s)
-	}
-	log.Println(msg)
+	log.Println(append([]interface{}{"[Err]"}, v...)...)
 }
 
 func Warn(v ...interface{}) {
-	msg := "[Warn]"
-	for _, s := range v {
-		msg += " " + fmt.Sprint(s)
-	}
-	log.Println(msg)
+	log.Println(append([]interface{}{"[Warn]"}, v...)...)
 }
 
 func Info(v ...interface{}) {
-	msg := "[Info]"
-	for _, s := range v {
-		msg += " " + fmt.Sprint(s)
-	}
-	log.Println(msg)
+	log.Println(append([]interface{}{"[Info]"}, v...)...)
 }
